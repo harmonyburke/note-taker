4c7c5cec34bbfd7984d9582fb70266f48b31e0d5
Harmony Burke <theharmonyburke@gmail.com>
2023-11-30 18:24:23 -0600

    fixed get and started post req

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const PORT=3001;
 // specify port
 app.use(express.static('public'));
 // middleware pointing to public folder
+const path = require('path');
+
 app.get('/notes', (req,res) =>
-res.sendFile(path.join(__dirname, 'public/notes.html')))
+res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req,res) =>
+res.sendFile(path.join(__dirname, 'db/db.json')));
 // sends response to notes.html
-app.post('/notes', (req,res) =>{
-    
+app.post('/api/notes', (req,res) =>{
+const {body}=req
 }
 )
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
